99c17e63183223507ed25fd06ca4774ad7157286
eledx <eleonore.dupoix@gmail.com>
2019-02-10 08:48:23 +0100

    ++

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/starwars/')));
+app.use(express.static('./dist/starwars'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/starwars/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/starwars/index.html'));
 });
 
 const port = process.env.PORT || '3001';
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('Server running'));
+app.listen(port, () => console.log('Server running'));
